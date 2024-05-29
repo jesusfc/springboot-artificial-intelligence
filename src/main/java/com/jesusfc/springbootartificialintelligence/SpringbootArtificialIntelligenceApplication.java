a97e32abe47139514535b4dbac557b0a1fd5d06f
jfcaraballo <jfcaraballo@gmail.com>
2024-05-29 14:04:59 +0200

    Initial commit

---
+++ src/main/java/com/jesusfc/springbootartificialintelligence/SpringbootArtificialIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.jesusfc.springbootartificialintelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootArtificialIntelligenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootArtificialIntelligenceApplication.class, args);
+    }
+
+}
